cac87608a8251273a515e05c1718f9a92afbb0e3
Andrea Garau <andreagarau98@gmail.com>
2017-08-28 11:03:22 +0200

    Created the interface. Defined the GoalCell with all the outlets and methods.

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Andrea on 28/08/2017.
+//  Copyright © 2017 Andrea. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
+    
+
+}
+
