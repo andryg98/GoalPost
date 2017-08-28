d77129620f8c7dacf7bd4bacaf07c168f23fa330
Andrea Garau <andreagarau98@gmail.com>
2017-08-28 14:40:08 +0200

    Added the tableView and made some test to verify that it works

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Andrea on 28/08/2017.
+//  Copyright © 2017 Andrea. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case shortTerm = "Short Term"
+    case longTerm = "Long Term"
+}
