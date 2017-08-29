0a9c5daf2261f3f377410717e993a78b639f922c
Andrea Garau <andreagarau98@gmail.com>
2017-08-29 12:51:37 +0200

    Implemented the save function that saves data inside the persistent container.

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 class FinishGoalVC: UIViewController {
 
@@ -16,12 +18,15 @@ class FinishGoalVC: UIViewController {
     var goalDescription: String!
     var goalType: GoalType!
     
-    
+    //Initialize data starting from the data in CreateGoalVC
     func initData(description: String, type: GoalType) {
         self.goalDescription = description
         self.goalType = type
     }
     
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +35,33 @@ class FinishGoalVC: UIViewController {
 
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
         //Pass data into the Core Data Goal Model
+        if pointsTextField.text != nil {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+        
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("Successfully inserted data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
     }
     
 }
